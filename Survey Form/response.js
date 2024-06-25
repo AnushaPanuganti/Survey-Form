5fce6b8e496e5ec4e2792f1798c98d7d403e75a7
AnushaPanuganti <107758299+AnushaPanuganti@users.noreply.github.com>
2024-06-26 03:22:55 +0530

    commiting survey form

---
+++ Survey Form/response.js
@@ -0,0 +1,8 @@
+function checkCourse(text) {
+    var course = document.querySelector('#course').value;
+    if (course == "") {
+        alert("Please mention any programming language that you are interested to learn further");
+        return false;
+    }
+    
+}
